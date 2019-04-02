359feccb2fd62974af50346cd559c5847ba964d8
anubhav2012 <49182328+anubhav2012@users.noreply.github.com>
2019-04-02 12:04:25 +0530

    Update prog1.c

--- prog1.c
+++ prog1.c
@@ -1,5 +1,18 @@
-#include<stdio.h>
-void main()
-{ 
-	printf("Hello Python");
+#include <stdio.h>
+int main()
+{
+    int i, j, rows;
+
+    printf("Enter number of rows: ");
+    scanf("%d",&rows);
+
+    for(i=1; i<=rows; ++i)
+    {
+        for(j=1; j<=i; ++j)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
 }
